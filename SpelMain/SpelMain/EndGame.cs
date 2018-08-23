1890110fd38ea47895a54e9b04f2042ba5edb693
Jesper Eriksson <jesper--eriksson@hotmail.com>
2018-08-23 10:52:14 +0200

    s

--- SpelMain/SpelMain/EndGame.cs
+++ SpelMain/SpelMain/EndGame.cs
@@ -4,12 +4,12 @@ using System.Text;
 
 namespace SpelMain
 {
-    class EndGame
+   public class EndGame
     {
-        public static void ThankPlayerForPlaying()
+        public void ThankPlayerForPlaying()
         {
             Console.WriteLine("You completed the game!!");
-            Console.WriteLine(CreatePlayer.Name + "you saved the village from a disaster!");
+            Console.WriteLine(CreatePlayer.NameOfPlayer + "you saved the village from a disaster!");
             Console.WriteLine("Send us some cash if you liked the game and want to play some more");
 
         }
