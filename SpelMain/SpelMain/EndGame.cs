987733b7e0dc371eef9af47b3c879bc8826b25ff
Jesper Eriksson <jesper--eriksson@hotmail.com>
2018-08-23 10:17:18 +0200

    a

---
+++ SpelMain/SpelMain/EndGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpelMain
+{
+    class EndGame
+    {
+        public static void ThankPlayerForPlaying()
+        {
+            Console.WriteLine("You completed the game!!");
+            Console.WriteLine(CreatePlayer.Name + "you saved the village from a disaster!");
+            Console.WriteLine("Send us some cash if you liked the game and want to play some more");
+
+        }
+
+
+    }
+}
